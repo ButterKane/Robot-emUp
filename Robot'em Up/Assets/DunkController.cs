5de257ca5bf3f49560a2fdcca55f62dee678fca0
Marolles <julesduvette@gmail.com>
2019-10-17 15:49:54 +0200

    Added dunk

--- Robot'em Up/Assets/DunkController.cs
+++ Robot'em Up/Assets/DunkController.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using XInputDotNetPure;
 
+public enum DunkState
+{
+	Jumping,
+	Waiting,
+	Dashing,
+	Cancelling,
+	None
+}
 public class DunkController : MonoBehaviour
 {
 	[Header("Settings")]
@@ -19,25 +27,54 @@ public class DunkController : MonoBehaviour
 	public float dunkCancelledFallSpeed;
 
 
+	private Rigidbody rb;
+	[SerializeField] private DunkState dunkState;
+	private Coroutine jumpCoroutine;
 
-
-	public void DunkJump() //Player goes to the skies and waits for the ball
+	private void Awake ()
 	{
-		StartCoroutine(DunkJump_C());
+		rb = GetComponent<Rigidbody>();
 	}
 
-	public void DunkCancel () //Player stayed some time in the skies and got no ball, so dunk is cancelled :( 
+	public void Explode()
 	{
 
 	}
 
-	public void DunkDash() //Player got a ball while in the skies, now it's show time
+	public void OnBallReceive()
 	{
+		if (jumpCoroutine != null && dunkState == DunkState.Waiting)
+		{
+			StopCoroutine(jumpCoroutine);
+		}
+		StartCoroutine(DunkOnGround_C());
+	}
 
+	public void Dunk()
+	{
+		jumpCoroutine = StartCoroutine(Dunk_C());
+	}
+
+	IEnumerator Dunk_C()
+	{
+		yield return DunkJump_C();
+		yield return DunkWait_C();
+		yield return DunkCancel_C();
+	}
+
+	IEnumerator DunkOnGround_C ()
+	{
+		ChangeState(DunkState.Dashing);
+		yield return FallOnGround_C(dunkDashSpeed);
+		Explode();
 	}
 
 	IEnumerator DunkJump_C()
 	{
+		ChangeState(DunkState.Jumping);
+		rb.isKinematic = true;
+		rb.useGravity = false;
+
 		Vector3 startPosition = transform.position;
 		Vector3 endPosition = startPosition + Vector3.up * dunkJumpHeight + transform.forward * dunkJumpLength;
 
@@ -46,6 +83,66 @@ public class DunkController : MonoBehaviour
 			transform.position = Vector3.Lerp(startPosition, endPosition, i / dunkJumpDuration);
 			yield return new WaitForEndOfFrame();
 		}
-		yield return null;
+
+		transform.position = endPosition;
+	}
+
+	IEnumerator DunkWait_C()
+	{
+		ChangeState(DunkState.Waiting);
+		for (float i = 0; i < dunkJumpFreezeDuration; i+= Time.deltaTime)
+		{
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	IEnumerator DunkCancel_C()
+	{
+		ChangeState(DunkState.Cancelling);
+		yield return FallOnGround_C(dunkCancelledFallSpeed);
+
+	}
+	IEnumerator FallOnGround_C(float _speed)
+	{
+		Vector3 startPosition = transform.position;
+		Vector3 endPosition = startPosition;
+
+		//Raycast to the ground to find end position
+		RaycastHit hit;
+		if (Physics.Raycast(startPosition, Vector3.down * 50, out hit, Mathf.Infinity, LayerMask.GetMask("Environment")))
+		{
+			endPosition = hit.point;
+		} else
+		{
+			StopAllCoroutines();
+			ChangeState(DunkState.None);
+		}
+
+		for (float i = 0; i < Vector3.Distance(startPosition, endPosition); i+=Time.deltaTime * _speed)
+		{
+			transform.position = Vector3.Lerp(startPosition, endPosition, i / Vector3.Distance(startPosition, endPosition));
+			yield return new WaitForEndOfFrame();
+		}
+		transform.position = endPosition;
+
+		ChangeState(DunkState.None);
+		rb.isKinematic = false;
+		rb.useGravity = true;
+	}
+
+	private void ChangeState(DunkState _newState)
+	{
+		switch (_newState)
+		{
+			case DunkState.Jumping:
+				break;
+			case DunkState.Dashing:
+				break;
+			case DunkState.Waiting:
+				break;
+			case DunkState.Cancelling:
+				break;
+		}
+		dunkState = _newState;
 	}
 }
