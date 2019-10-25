d1a9d65f23a7abcc66ad81f9ded9e278e0384ed1
Matthieu <m.riboulot@rubika-edu.com>
2019-10-25 11:40:19 +0200

    Feat: Improve Activator/Activable structure

--- Robot'em Up/Assets/Scripts/Behaviours/PuzzleActivable.cs
+++ Robot'em Up/Assets/Scripts/Behaviours/PuzzleActivable.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PuzzleActivable : MonoBehaviour
 {
+    public PuzzleDatas puzzleData;
     [SerializeField] public List<PuzzleActivator> puzzleActivators;
     [SerializeField] public List<PuzzleActivator> puzzleDesactivator;
+    [SerializeField] public bool needAllConditions = false;
     public virtual void WhenActivate()
     { }
 
