da27d13ef77df1d2b345d73ab2e66dd32e57aa68
Matthieu <m.riboulot@rubika-edu.com>
2019-10-30 14:35:02 +0100

    Feat: logic basis for pressure plate

---
+++ Robot'em Up/Assets/Scripts/Puzzle Behaviors/PuzzlePressurePlate.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzlePressurePlate : PuzzleActivator
+{
+    private bool PlayerHere;
+    private BoxCollider boxCollider;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!PlayerHere)
+        {
+            if (other.GetComponent<PlayerController>())
+            {
+                PlayerHere = true;
+                ActivateLinkedObjects();
+                transform.localScale = new Vector3(transform.localScale.x, 0.1f, transform.localScale.z);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (PlayerHere)
+        {
+            if (other.GetComponent<PlayerController>())
+            {
+                DesactiveLinkedObjects();
+                PlayerHere = false;
+                transform.localScale = new Vector3(transform.localScale.x, 1f, transform.localScale.z);
+            }
+        }
+    }
+}
