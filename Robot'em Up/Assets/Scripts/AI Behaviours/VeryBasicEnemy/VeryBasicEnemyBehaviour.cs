f0c58d0d4b235cd7e10a1b46b2a9cabacf234e76
Quentin Roze <quentin.roze@gmail.com>
2019-11-14 19:20:17 +0100

    VeryBasicEnemy update : can attack but no damage

---
+++ Robot'em Up/Assets/Scripts/AI Behaviours/VeryBasicEnemy/VeryBasicEnemyBehaviour.cs
@@ -0,0 +1,307 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MyBox;
+using UnityEngine.AI;
+
+public enum VeryBasicEnemyState
+{
+    Idle,
+    Following,
+    Staggering,
+    ChangingFocus,
+    PreparingAttack,
+    Attacking,
+    PauseAfterAttack,
+    Dying,
+}
+
+public class VeryBasicEnemyBehaviour : MonoBehaviour,IHitable
+{
+    VeryBasicEnemyState State = VeryBasicEnemyState.Idle;
+
+    [Separator("References")]
+    [SerializeField] private Transform _self;
+    public Rigidbody Rb;
+    public Animator Animator;
+    public NavMeshAgent navMeshAgent;
+
+    [Space(2)]
+    [Separator("Auto-assigned References")]
+    public Transform Target;
+    [SerializeField] private Transform _playerOne;
+    [SerializeField] private Transform _playerTwo;
+
+
+    bool playerOneInRange;
+    bool playerTwoInRange;
+    public int MaxHealth = 100;
+    public int Health;
+    float distanceWithPlayerOne;
+    float distanceWithPlayerTwo;
+    float distanceWithFocusedPlayer;
+    Transform focusedPlayer = null;
+    
+    public float focusDistance;
+    public float unfocusDistance;
+    public float timeBetweenCheck;
+    public float distanceBeforeChangingPriority;
+
+    public float distanceToAttack;
+    public float maxAnticipationTime;
+    float anticipationTime;
+    public float attackMaxDistance;
+    public float maxAttackDuration;
+    float attackDuration;
+    float attackTimeProgression;
+    public AnimationCurve attackSpeedCurve;
+    Vector3 attackInitialPosition;
+    Vector3 attackDestination;
+    [Range(0, 1)] public float whenToTriggerNextAnim;
+
+    public float maxTimePauseAfterAttack;
+    float timePauseAfterAttack;
+    float azdazd;
+
+
+    void Start()
+    {
+        _self = transform;
+        _playerOne = GameManager.i.playerOne.transform;
+        _playerTwo = GameManager.i.playerTwo.transform;
+        State = VeryBasicEnemyState.Following;
+        StartCoroutine(CheckDistance());
+    }
+    
+    void Update()
+    {
+        UpdateDistancesToPlayers();
+        UpdateState();
+        UpdateAnimatorBlendTree();
+    }
+
+    private void UpdateAnimatorBlendTree()
+    {
+        if (Animator != null)            
+        {
+            Animator.SetFloat("IdleRunBlend", navMeshAgent.velocity.magnitude / navMeshAgent.speed);
+        }
+    }
+
+    void UpdateState()
+    {
+        switch (State)
+        {
+            case VeryBasicEnemyState.Idle:
+                break;
+            case VeryBasicEnemyState.Following:
+                if(focusedPlayer != null)
+                {
+                    navMeshAgent.SetDestination(focusedPlayer.position);
+                    if (distanceWithFocusedPlayer <= distanceToAttack)
+                    {
+                        ChangingState(VeryBasicEnemyState.PreparingAttack);
+                    }
+                }
+                break;
+            case VeryBasicEnemyState.Staggering:
+                break;
+            case VeryBasicEnemyState.ChangingFocus:
+                break;
+            case VeryBasicEnemyState.PreparingAttack:
+                anticipationTime -= Time.deltaTime;
+                azdazd += Time.deltaTime;
+                if (anticipationTime <= 0)
+                {
+                    ChangingState(VeryBasicEnemyState.Attacking);
+                }
+                break;
+            case VeryBasicEnemyState.Attacking:
+                attackTimeProgression += Time.deltaTime / attackDuration;
+                Rb.MovePosition(Vector3.Lerp(attackInitialPosition, attackDestination, attackSpeedCurve.Evaluate(attackTimeProgression)));
+                attackDuration -= Time.deltaTime;
+                if(attackDuration <= 0)
+                {
+                    ChangingState(VeryBasicEnemyState.PauseAfterAttack);
+                }
+                else if(attackDuration <= whenToTriggerNextAnim)
+                {
+                    Animator.SetTrigger("EndOfAttackTrigger");
+                }
+                break;
+            case VeryBasicEnemyState.PauseAfterAttack:
+                timePauseAfterAttack -= Time.deltaTime;
+                if(timePauseAfterAttack <= 0)
+                {
+                    ChangingState(VeryBasicEnemyState.Following);
+                }
+                break;
+            case VeryBasicEnemyState.Dying:
+                break;
+        }
+    }
+
+    public void ChangingState(VeryBasicEnemyState _newState)
+    {
+        ExitState();
+        State = _newState;
+        EnterState();
+    }
+
+    void EnterState()
+    {
+        print(State);
+        switch (State)
+        {
+            case VeryBasicEnemyState.Idle:
+                break;
+            case VeryBasicEnemyState.Following:
+                navMeshAgent.enabled = true;
+                break;
+            case VeryBasicEnemyState.Staggering:
+                break;
+            case VeryBasicEnemyState.ChangingFocus:
+                break;
+            case VeryBasicEnemyState.PreparingAttack:
+                navMeshAgent.enabled = false;
+                anticipationTime = maxAnticipationTime;
+                azdazd = 0;
+                Animator.SetTrigger("AttackTrigger");
+                break;
+            case VeryBasicEnemyState.Attacking:
+                attackDuration = maxAttackDuration;
+                attackInitialPosition = transform.position;
+                attackDestination = attackInitialPosition + attackMaxDistance*transform.forward;
+                attackTimeProgression = 0;
+                break;
+            case VeryBasicEnemyState.PauseAfterAttack:
+                timePauseAfterAttack = maxTimePauseAfterAttack;
+                break;
+            case VeryBasicEnemyState.Dying:
+                break;
+        }
+    }
+
+    void ExitState()
+    {
+        switch (State)
+        {
+            case VeryBasicEnemyState.Idle:
+                break;
+            case VeryBasicEnemyState.Following:
+                break;
+            case VeryBasicEnemyState.Staggering:
+                break;
+            case VeryBasicEnemyState.ChangingFocus:
+                break;
+            case VeryBasicEnemyState.PreparingAttack:
+                break;
+            case VeryBasicEnemyState.Attacking:
+                break;
+            case VeryBasicEnemyState.PauseAfterAttack:
+                break;
+            case VeryBasicEnemyState.Dying:
+                break;
+        }
+    }
+
+    void UpdateDistancesToPlayers()
+    {
+        distanceWithPlayerOne = Vector3.Distance(_self.position, _playerOne.position);
+        distanceWithPlayerTwo = Vector3.Distance(_self.position, _playerTwo.position);
+        if(focusedPlayer != null)
+            distanceWithFocusedPlayer = Vector3.Distance(_self.position, focusedPlayer.position);
+    }
+
+    Transform GetClosestPlayer()
+    {
+        if (distanceWithPlayerOne >= distanceWithPlayerTwo)
+        {
+            return _playerTwo;
+        }
+        else
+        {
+            return _playerOne;
+        }
+    }
+
+    public void OnHit(BallBehaviour _ball, Vector3 _impactVector, PawnController _thrower, int _damages, DamageSource _source)
+    {
+        Health -= _damages;
+        if(Health <= 0)
+        {
+            Die();
+        }
+        throw new System.NotImplementedException();
+    }
+
+    void Die()
+    {
+        print("hey");
+        Destroy(gameObject);
+    }
+
+    IEnumerator CheckDistance()
+    {
+        //Checking who is in range
+        if (distanceWithPlayerOne < focusDistance)
+        {
+            playerOneInRange = true;
+        }
+        else
+        {
+            playerOneInRange = false;
+        }
+
+        if (distanceWithPlayerTwo < focusDistance)
+        {
+            playerTwoInRange = true;
+        }
+        else
+        {
+            playerTwoInRange = false;
+        }
+
+        //Unfocus player because of distance
+        if (focusedPlayer != null)
+        {
+            if ((focusedPlayer == _playerOne && distanceWithPlayerOne > unfocusDistance) || (focusedPlayer == _playerTwo && distanceWithPlayerTwo > unfocusDistance))
+            {
+                ChangingFocus(null);
+            }
+        }
+
+        //Changing focus between the two
+        if (playerOneInRange && playerTwoInRange && focusedPlayer != null)
+        {
+            if (focusedPlayer == _playerOne && distanceWithPlayerOne - distanceWithPlayerTwo > distanceBeforeChangingPriority)
+            {
+                ChangingFocus(_playerTwo);
+            }
+            else if (focusedPlayer == _playerTwo && distanceWithPlayerTwo - distanceWithPlayerOne > distanceBeforeChangingPriority)
+            {
+                ChangingFocus(_playerOne);
+            }
+        }
+
+        //no focused yet ? Choose one
+        if ((playerOneInRange || playerTwoInRange) && focusedPlayer == null)
+        {
+            ChangingFocus(GetClosestPlayer());
+        }
+
+        //Restart coroutine in X seconds
+        yield return new WaitForSeconds(timeBetweenCheck);
+        StartCoroutine(CheckDistance());
+    }
+
+    void ChangingFocus(Transform _newFocus)
+    {
+        /*if(_newFocus != null)
+        {
+
+        }*/
+        focusedPlayer = _newFocus;
+    }
+
+}
