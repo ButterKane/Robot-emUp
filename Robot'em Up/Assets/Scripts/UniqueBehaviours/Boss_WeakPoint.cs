ae5c56cccc4c3f0dafef62e29bd3c920ecc96292
Matthieu <m.riboulot@rubika-edu.com>
2019-11-20 12:19:12 +0100

    Feat: Intermediate iteration

---
+++ Robot'em Up/Assets/Scripts/UniqueBehaviours/Boss_WeakPoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_WeakPoint : PuzzleActivable
+{
+    // Start is called before the first frame update
+
+
+    public GameObject weakPointModel;
+    public GameObject explosionFx;
+
+    override public void WhenActivate()
+    {
+        isActivated = true;
+        UpdateLights();
+        DestroyWeakPoint();
+
+    }
+
+    public void DestroyWeakPoint()
+    {
+        if (weakPointModel != null)
+        {
+            Destroy(weakPointModel);
+            FXManager.InstantiateFX(explosionFx, Vector3.up * 1, true, Vector3.zero, Vector3.one * 2);
+        }
+    }
+}
