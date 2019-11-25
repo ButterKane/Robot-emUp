090e44870c11d2b113dde42d226cec47030d4d23
Marolles <julesduvette@gmail.com>
2019-11-25 15:35:21 +0100

    Removed player & ball cursor Added ballPointer to see where is the ball

--- Robot'em Up/Assets/CursorManager.cs
+++ Robot'em Up/Assets/CursorManager.cs
@@ -5,54 +5,48 @@ using UnityEngine.UI;
 
 public class CursorManager : MonoBehaviour
 {
-    public Camera MainCamera;
-    public Image Player1Cursor;
-    public Image Player2Cursor;
-    public Image BallCursor;
-    
-
-    private PlayerController _playerOne;
-    private PlayerController _playerTwo;
-    private Transform _ball;
+	public GameObject BallPointerPrefab;
+	private static GameObject ballPointer;
+	private static Transform ballPointerParent;
 
     // Start is called before the first frame update
     void Start()
     {
-        _playerOne = GameManager.i.playerOne;
-        _playerTwo = GameManager.i.playerTwo;
-        _ball = GameManager.i.ball.transform;
+		ballPointer = Instantiate(BallPointerPrefab);
+		ballPointer.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-		if (_playerOne.IsTargetable())
+		if (ballPointer && ballPointerParent)
 		{
-			Player1Cursor.enabled = true;
-			Player1Cursor.transform.position = MainCamera.WorldToScreenPoint(_playerOne.transform.position);
-		} else
-		{
-			Player1Cursor.enabled = false;
+			ballPointer.transform.position = ballPointerParent.transform.position;
 		}
-
-		if (_playerTwo.IsTargetable())
-		{
-			Player1Cursor.enabled = true;
-			Player2Cursor.transform.position = MainCamera.WorldToScreenPoint(_playerTwo.transform.position);
-		} else
-		{
-			Player1Cursor.enabled = false;
-		}
-
-        if (_ball == null )
-        {
-            _ball = GameManager.i.ball.transform;
-            Debug.Log("_ball null");
-        }
-        BallCursor.transform.position = MainCamera.WorldToScreenPoint(_ball.position);
-
-        Player1Cursor.transform.position = new Vector3(Player1Cursor.transform.position.x, Player1Cursor.transform.position.y + 20, Player1Cursor.transform.position.z);
-        Player2Cursor.transform.position = new Vector3(Player2Cursor.transform.position.x, Player2Cursor.transform.position.y + 20, Player2Cursor.transform.position.z);
-        BallCursor.transform.position = new Vector3(BallCursor.transform.position.x, BallCursor.transform.position.y + 20, BallCursor.transform.position.z);
     }
+
+	public static void ShowBallPointer(bool state)
+	{
+		switch (state)
+		{
+			case true:
+				ballPointer.SetActive(true);
+				break;
+			case false:
+				ballPointer.SetActive(false);
+				break;
+		}
+	}
+
+	public static void SetBallPointerParent(Transform newParent)
+	{
+		if (newParent == null) {
+			ShowBallPointer(false);
+			ballPointerParent = null;
+		} else
+		{
+			ShowBallPointer(true);
+			ballPointerParent = newParent;
+		}
+	}
 }
