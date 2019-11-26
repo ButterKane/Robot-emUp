905a3bdbafaff13701a9db5c91fcf4c2b1e01dee
ButterKane <alexandre.fourquet@gmail.com>
2019-11-26 17:03:12 +0100

    feat: cheat buttons in Managers prefab's script

---
+++ Robot'em Up/Assets/Scripts/Tools/CheatCodes.cs
@@ -0,0 +1,51 @@
+﻿using MyBox;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatCodes : MonoBehaviour
+{
+    public bool ActivateCheat;
+    [ConditionalField(nameof(ActivateCheat))] public GameObject BallPrefab;
+    [ConditionalField(nameof(ActivateCheat))] public PlayerController PlayerOne;
+    [ConditionalField(nameof(ActivateCheat))] public PlayerController PlayerTwo;
+    [ConditionalField(nameof(ActivateCheat))] public bool playersInvicible;
+
+    private void Start()
+    {
+        BallPrefab = GameManager.i.ballPrefab;
+        PlayerOne = GameManager.i.playerOne;
+        PlayerTwo = GameManager.i.playerTwo;
+    }
+
+    void OnGUI()
+    {
+        if (ActivateCheat)
+        {
+            if (GUI.Button(new Rect(10, 10, 60, 25), "TP Ball"))
+            {
+                TPBallOnPlayer();
+            }
+            if (GUI.Button(new Rect(10, 40, 120, 25), "Toggle invicibility"))
+            {
+                ToggleInvicibility();
+            }
+        }
+    }
+
+
+    public void TPBallOnPlayer()
+    {
+        if (GameManager.i.ball != null)
+        {
+            Destroy(GameManager.i.ball);
+        }
+
+        Instantiate(BallPrefab, PlayerOne.transform.position, Quaternion.identity);
+    }
+
+    public void ToggleInvicibility()
+    {
+        // TO DO: activate invicibility
+    }
+}
