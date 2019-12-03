bc3ebe488c18c04bb8ee55d89eb9daf57247ba41
Marolles <julesduvette@gmail.com>
2019-12-03 11:20:02 +0100

    Added player UI default values

---
+++ Robot'em Up/Assets/Editor/PlayerUIEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlayerUI))]
+public class PlayerUIEditor : Editor
+{
+	PlayerUI playerUI;
+
+	private void OnEnable ()
+	{
+		playerUI = (PlayerUI)target;
+	}
+
+	public override void OnInspectorGUI ()
+	{
+
+	}
+}
--- Robot'em Up/Assets/PlayerUI.cs
+++ Robot'em Up/Assets/PlayerUI.cs
@@ -1,16 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerUI : MonoBehaviour
 {
+
+	//Global settings
 	public bool displayHealth;
 	public bool displayDashes;
 
+	//Health settings
+	public float healthFadeInSpeed;
+	public float healthFadeOutSpeed;
+	public float healthShowDuration;
+	public float healthGainStartScale;
+	public float healthGainEndScale;
+	public float healthLosseStartScale;
+	public float healthLosseEndScale;
+	public AnimationCurve healthAnimationCurve;
+
+	//Dash settings
+	public float dashFadeInSpeed;
+	public float dashFadeOutSpeed;
+	public float dashShowDuration;
+	public Image dashBar;
+	public Color dashBarColor;
 
 
-
-	public void DisplayHealth(float _duration, float _fadeInSpeed, float _fadeOutSpeed )
+	public void DisplayHealth(float _duration, float _fadeInSpeed, float _fadeOutSpeed, float _startScale, float _endScale, AnimationCurve _scaleCurve )
 	{
 
 	}
