ebb020cca4b6171881cb8927a2db830aa9ed2d26
ButterKane <alexandre.fourquet@gmail.com>
2019-12-04 11:53:09 +0100

    Chore: Adding some "pense-bète" for later. It's not used anywhere so no problem

---
+++ Robot'em Up/Assets/Debug/Tests Scriptable + animator/TestEnemyProfile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Enemy Profile.asset", menuName = "Enemy Profile", order = 100)]
+public class TestEnemyProfile : ScriptableObject
+{
+    public float speed;
+    public Color color;
+    public AnimationCurve acceleration;
+}
