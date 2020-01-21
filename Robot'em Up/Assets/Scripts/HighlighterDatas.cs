2e94d490531109fdb4278a72e46d8e657a65709f
Marolles <julesduvette@gmail.com>
2020-01-21 17:31:20 +0100

    Added highlighter (Using LB), dunk energy loss, new input for dunk, reworked lock, fixed bug (player being able to use inputs while dead)

---
+++ Robot'em Up/Assets/Scripts/HighlighterDatas.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HighlighterDatas", menuName = "GlobalDatas/HighlighterDatas", order = 1)]
+public class HighlighterDatas : ScriptableObject
+{
+	public float duration;
+	public float fadeInDuration;
+	public float fadeOutDuration;
+
+	public Color defaultFirstColor = Color.white;
+	public Color defaultSecondColor = Color.white;
+
+	public float arrowSpawnHeight;
+	public float arrowMinZOffset;
+	public float arrowMaxZOffset;
+	public float arrowHoverSpeed;
+
+	public float circleZOffset;
+	public float circleMinSize;
+	public float circleMaxSize;
+	public float circleAnimationSpeed;
+
+	public float minEmissionForce;
+	public float maxEmissionForce;
+	public float emissionLerpSpeed;
+}
---
+++ Robot'em Up/Assets/Scripts/MaterialReplacer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialReplacer : MonoBehaviour
+{
+	Renderer renderer;
+	Material[] previousMaterials;
+	public void ReplaceMaterial ( Material _newMaterial)
+	{
+		renderer = GetComponent<Renderer>();
+		previousMaterials = renderer.materials;
+		renderer.material = _newMaterial;
+	}
+
+	private void OnDestroy ()
+	{
+		if (renderer != null)
+		{
+			renderer.materials = previousMaterials;
+		}
+	}
+}
