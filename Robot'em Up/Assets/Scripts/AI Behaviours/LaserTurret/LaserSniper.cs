09508ffdd4340f94ed8d0003fdbb959c72ed9de6
ButterKane <alexandre.fourquet@gmail.com>
2020-01-22 17:22:17 +0100

    feat: laserSniper fires a laser, and it adapts with what it touches

--- Robot'em Up/Assets/Scripts/AI Behaviours/LaserTurret/LaserSniper.cs
+++ Robot'em Up/Assets/Scripts/AI Behaviours/LaserTurret/LaserSniper.cs
@@ -4,40 +4,38 @@ using UnityEngine;
 
 public class LaserSniper : MonoBehaviour
 {
-    public Rigidbody rb;
     public float speed;
-    public int damageDealt;
-    public float maxLifeTime;
+    [HideInInspector] public int damageDealt;
     Vector3 initialPosition;
     [HideInInspector] public Transform target;
     [HideInInspector] public Transform spawnParent;
-    private LaserSniperBehaviour enemyScript;
+    [HideInInspector] public LaserSniperTurretBehaviour enemyScript;
     public GameObject impactFX;
     public Vector3 impactFXScale;
     public LayerMask layerToCheck;
     public float distanceToRaycast;
-    public bool canHitEnemies;
+    private float? laserLength = null;
 
     [HideInInspector] public bool isAimingPlayer;
-    public float distanceAoEDamage;
+    [HideInInspector] public float distanceAoEDamage;
 
     private void Start()
     {
         initialPosition = transform.position;
-        enemyScript = spawnParent.GetComponent<LaserSniperBehaviour>();
     }
-    
+
     void Update()
     {
         //rb.velocity = (target.position - transform.position).normalized * speed;
-        maxLifeTime -= Time.deltaTime;
-        if (maxLifeTime <= 0 || !target.GetComponent<PawnController>().IsTargetable())
-        {
-            Destroy(gameObject);
-        }
+        //maxLifeTime -= Time.deltaTime;
+        //if (maxLifeTime <= 0 || !target.GetComponent<PawnController>().IsTargetable())
+        //{
+        //    Destroy(gameObject);
+        //}
 
         RaycastHit hit;
-        if(Physics.Raycast(transform.position, rb.velocity, out hit, distanceToRaycast, layerToCheck)){
+        if (Physics.Raycast(transform.position, transform.forward, out hit, distanceToRaycast, layerToCheck))
+        {
             GameObject i_impactFX = Instantiate(impactFX, hit.point, Quaternion.identity);
             i_impactFX.transform.localScale = impactFXScale;
             Destroy(i_impactFX, .25f);
@@ -45,10 +43,51 @@ public class LaserSniper : MonoBehaviour
             {
                 target.GetComponent<PawnController>().Damage(damageDealt);
             }
-            Destroy(gameObject);
         }
+            RaycastToHitWithLaser();
+        UpdateLaserLength(laserLength);
 
         //UpdateLaserPosition();
+    }
+
+    public void UpdateLaserLength(float? givenLength)
+    {
+        float i_laserLength = 0;
+        if (givenLength == null)
+        {
+            i_laserLength = enemyScript.laserMaxLength;
+        }
+        else
+        {
+            i_laserLength = (float)givenLength;
+        }
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, i_laserLength);
+    }
+
+    public void RaycastToHitWithLaser()
+    {
+        RaycastHit[] i_hitObjects = Physics.RaycastAll(transform.position, transform.TransformDirection(Vector3.forward), enemyScript.laserMaxLength);
+        if (i_hitObjects.Length > 0)
+        {
+            foreach (var touched in i_hitObjects)
+            {
+                IHitable i_potentialHitableObject = touched.transform.GetComponent<IHitable>();
+                if (i_potentialHitableObject != null)
+                {
+                    i_potentialHitableObject.OnHit(null, (touched.transform.position - transform.position).normalized, null, enemyScript.damagePerSecond / 60, DamageSource.Laser, Vector3.zero);
+                }
+
+                if (touched.transform.tag == "Player" || touched.transform.tag == "Environment")
+                {
+                    laserLength = touched.distance;
+                }
+            }
+        }
+        else // if nothing has been hit (no wall, no player, nada)
+        {
+            laserLength = enemyScript.laserMaxLength;
+        }
+
     }
 
     //public void UpdateLaserPosition()
@@ -59,7 +98,7 @@ public class LaserSniper : MonoBehaviour
 
     private void OnTriggerEnter(Collider _other)
     {
-        if (_other.tag == "Enemy" && canHitEnemies)
+        if (_other.tag == "Enemy")
         {
             _other.GetComponent<PawnController>().Damage(damageDealt);
             GameObject i_impactFX = Instantiate(impactFX, transform.position, Quaternion.identity);
