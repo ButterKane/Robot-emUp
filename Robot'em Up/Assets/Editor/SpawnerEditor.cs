879402c4f8216392e121a2d62e560d85103a55b8
Marolles <julesduvette@gmail.com>
2020-01-27 17:25:03 +0100

    Added spawner editor

---
+++ Robot'em Up/Assets/Editor/SpawnerEditor.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Spawner))]
+public class SpawnerEditor : Editor
+{
+	private Spawner spawner;
+	private void OnEnable ()
+	{
+		spawner = (Spawner)target;
+	}
+
+	public override void OnInspectorGUI ()
+	{
+		this.serializedObject.Update();
+		SerializedProperty m_type = serializedObject.FindProperty("type");
+		EditorGUILayout.PropertyField(m_type);
+
+		Transform previousStartPosition = spawner.startPosition;
+		EditorGUI.BeginChangeCheck();
+		SerializedProperty m_startPosition = serializedObject.FindProperty("startPosition");
+		EditorGUILayout.PropertyField(m_startPosition);
+		serializedObject.ApplyModifiedProperties();
+		if (EditorGUI.EndChangeCheck())
+		{
+			if (previousStartPosition != null && previousStartPosition.GetComponent<SpawnStartPosition>() != null)
+			{
+				DestroyImmediate(previousStartPosition.GetComponent<SpawnStartPosition>());
+			}
+			if (spawner.startPosition != null)
+			{
+				spawner.startPosition.gameObject.AddComponent<SpawnStartPosition>().linkedSpawner = spawner;
+				spawner.RecalculateEndspawnLocation();
+			}
+		}
+
+		SerializedProperty m_delayBeforeBeingFree = serializedObject.FindProperty("delayBeforeBeingFree");
+		EditorGUILayout.PropertyField(m_delayBeforeBeingFree);
+
+		if (spawner.type == SpawnerType.Air)
+		{
+			SerializedProperty m_zonePreviewDuration = serializedObject.FindProperty("zonePreviewDuration");
+			EditorGUILayout.PropertyField(m_zonePreviewDuration);
+		}
+
+		SerializedProperty m_spawnDuration = serializedObject.FindProperty("spawnDuration");
+		EditorGUILayout.PropertyField(m_spawnDuration);
+
+		if (spawner.type == SpawnerType.Ground)
+		{
+			EditorGUI.BeginChangeCheck();
+			SerializedProperty m_jumpDistance = serializedObject.FindProperty("jumpDistance");
+			EditorGUILayout.PropertyField(m_jumpDistance);
+			SerializedProperty m_horizontalLerpCurve = serializedObject.FindProperty("horizontalLerpCurve");
+			EditorGUILayout.PropertyField(m_horizontalLerpCurve);
+			SerializedProperty m_rotationLerpCurve = serializedObject.FindProperty("rotationLerpCurve");
+			EditorGUILayout.PropertyField(m_rotationLerpCurve);
+			if (EditorGUI.EndChangeCheck())
+			{
+				spawner.RecalculateEndspawnLocation();
+			}
+		}
+
+		SerializedProperty m_verticalLerpCurve = serializedObject.FindProperty("verticalLerpCurve");
+		EditorGUILayout.PropertyField(m_verticalLerpCurve);
+		SerializedProperty m_delayBeforeActivation = serializedObject.FindProperty("delayBeforeActivation");
+		EditorGUILayout.PropertyField(m_delayBeforeActivation);
+		serializedObject.ApplyModifiedProperties();
+	}
+
+	public void OnSceneGUI ()
+	{
+		if (spawner.transform.hasChanged)
+		{
+			spawner.RecalculateEndspawnLocation();
+			spawner.transform.hasChanged = false;
+		}
+	}
+}
---
+++ Robot'em Up/Assets/Spawner.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public enum SpawnerType { Air, Underground, Ground }
+public class Spawner : MonoBehaviour
+{
+	public SpawnerType type;
+	public Transform startPosition;
+	public float delayBeforeBeingFree = 3;
+
+	public float zonePreviewDuration = 3;
+	public float spawnDuration = 2;
+	public float jumpDistance = 2;
+	public AnimationCurve horizontalLerpCurve;
+	public AnimationCurve rotationLerpCurve;
+	public AnimationCurve verticalLerpCurve;
+	public float delayBeforeActivation = 1;
+
+	public Vector3 endPosition;
+	public SpriteRenderer endPositionVisualizer;
+	public LineRenderer spawnVisualizer;
+
+	[ExecuteInEditMode]
+	public void RecalculateEndspawnLocation ()
+	{
+		if (endPositionVisualizer == null)
+		{
+			//Generates spawn vizualiser
+			GameObject spawnPositionVisualizer = new GameObject();
+			spawnPositionVisualizer.name = "EndPositionVizualiser";
+			spawnPositionVisualizer.transform.SetParent(transform);
+			endPositionVisualizer = spawnPositionVisualizer.AddComponent<SpriteRenderer>();
+			endPositionVisualizer.sprite = Resources.Load<Sprite>("ArenaResource/Spawners/spawnPositionVisualizer");
+			spawnPositionVisualizer.transform.localScale = Vector3.one * 0.2f;
+			spawnPositionVisualizer.transform.localPosition = Vector3.zero;
+			spawnPositionVisualizer.transform.rotation = Quaternion.LookRotation(Vector3.up);
+		}
+		if (spawnVisualizer == null)
+		{
+			spawnVisualizer = transform.gameObject.AddComponent<LineRenderer>();
+			spawnVisualizer.material = Resources.Load<Material>("ArenaResource/Spawners/spawnVisualizerMaterial");
+		}
+
+		if (startPosition == null)
+		{
+			Debug.LogWarning("Spawner doesn't have a start position, please add one");
+			return;
+		}
+		RaycastHit hit;
+		LayerMask layerMask = LayerMask.GetMask("Environment");
+		switch (type)
+		{
+			case SpawnerType.Air:
+				if (Physics.Raycast(startPosition.position, Vector3.down, out hit, Mathf.Infinity, layerMask))
+				{
+					endPosition = hit.point;
+				}
+				spawnVisualizer.positionCount = 2;
+				spawnVisualizer.SetPosition(0, startPosition.position);
+				spawnVisualizer.SetPosition(1, endPosition);
+				break;
+			case SpawnerType.Ground:
+				Vector3 forwardDirection = startPosition.transform.forward;
+				forwardDirection.y = 0;
+				forwardDirection = forwardDirection.normalized;
+				if (Physics.Raycast(startPosition.position + (forwardDirection * jumpDistance), Vector3.down, out hit, Mathf.Infinity, layerMask))
+				{
+					endPosition = hit.point;
+				}
+				spawnVisualizer.positionCount = 10;
+				for (int i = 0; i < 10; i++)
+				{
+					Vector3 horizontalPosition = Vector3.Lerp(startPosition.position, endPosition, horizontalLerpCurve.Evaluate((float)i / 10f));
+					Vector3 verticalPosition = Vector3.Lerp(startPosition.position, endPosition, verticalLerpCurve.Evaluate((float)i / 10f));
+					Vector3 finalPosition = horizontalPosition;
+					finalPosition.y = verticalPosition.y;
+					spawnVisualizer.SetPosition(i, finalPosition);
+				}
+				break;
+			case SpawnerType.Underground:
+				if (Physics.Raycast(startPosition.position + (Vector3.up * 50), Vector3.down, out hit, Mathf.Infinity, layerMask))
+				{
+					endPosition = hit.point;
+				}
+				spawnVisualizer.positionCount = 2;
+				spawnVisualizer.SetPosition(0, startPosition.position);
+				spawnVisualizer.SetPosition(1, endPosition);
+				break;
+		}
+		endPositionVisualizer.transform.position = endPosition + Vector3.up * 0.05f;
+	}
+}
