3759bf8cd44e1fa772c2c9cc592cf35b57c425a3
ButterKane <alexandre.fourquet@gmail.com>
2020-01-29 16:48:03 +0100

    fix: basic attack box disappears if enemy is bumped

--- Robot'em Up/Assets/Scripts/Player/PawnController.cs
+++ Robot'em Up/Assets/Scripts/Player/PawnController.cs
@@ -594,6 +594,9 @@ public class PawnController : MonoBehaviour
 
     private IEnumerator Bump_C()
     {
+        EnemyBehaviour enemy = GetComponent<EnemyBehaviour>();
+        enemy.ChangeState(EnemyState.Bumped);
+
         float i_bumpTimeProgression = 0;
         bool i_mustCancelBump = false;
 
@@ -645,9 +648,7 @@ public class PawnController : MonoBehaviour
 			gettingUpDuration -= Time.deltaTime;
             if (gettingUpDuration <= 0 && GetComponent<EnemyBehaviour>() != null)
 			{
-				EnemyBehaviour enemy = GetComponent<EnemyBehaviour>();
 				enemy.ChangeState(EnemyState.Following);
-				enemy.ExitBumpedState();
 			}
 			yield return null;
 		}
