88f95cd861c45b2c5451fa37af90ee303ca6a7f1
Marolles <julesduvette@gmail.com>
2020-02-18 17:34:09 +0100

    Added first boss functions and components

---
+++ Robot'em Up/Assets/Scripts/AI Behaviours/Boss Enemy (New)/BossBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BossBehaviour : MonoBehaviour
+{
+	public Transform topPart;
+	public float topPartRotationSpeed;
+	public GameObject healthBarPrefab;
+	private HealthBar healthBar1;
+	private HealthBar healthBar2;
+
+
+	private NavMeshAgent navMesh;
+
+	private void Awake ()
+	{
+		navMesh = GetComponent<NavMeshAgent>();
+		GenerateHealthBar();
+	}
+	private void Update ()
+	{
+		navMesh.SetDestination(GameManager.playerOne.transform.position);
+		Vector3 lookPosition = GameManager.playerOne.transform.position;
+		lookPosition.y = topPart.transform.position.y;
+		Quaternion wantedRotation = Quaternion.LookRotation(lookPosition - topPart.transform.position);
+		topPart.transform.rotation = Quaternion.Lerp(topPart.transform.rotation, wantedRotation, Time.deltaTime * topPartRotationSpeed);
+	}
+
+	public void Damage(float _amount)
+	{
+
+	}
+
+	public void GenerateHealthBar()
+	{
+
+	}
+}
--- Robot'em Up/Assets/Scripts/ProceduralSpiderLegAnimator.cs
+++ Robot'em Up/Assets/Scripts/ProceduralSpiderLegAnimator.cs
@@ -10,10 +10,15 @@ public class ProceduralSpiderLegAnimator : MonoBehaviour
 	public float height = 0.5f;
 	public float maxDistanceToTarget = 0.8f;
 	public AnimationCurve heightCurve;
-	public float legSpeed;
-	[HideInInspector] public bool canMove;
+	public AnimationCurve bumpHeightCurve;
+	public float bumpDuration = 0.5f;
+	public float bumpHeight = 4f;
+	public string eventOnStep;
+	[HideInInspector] public float legSpeed;
 	public bool isRight;
 	[HideInInspector] public bool isGrounded;
+	public Vector3 forwardOffset;
+	private Coroutine moveCoroutine;
 
 	private void Awake ()
 	{
@@ -28,7 +33,7 @@ public class ProceduralSpiderLegAnimator : MonoBehaviour
 
 	public void MoveLeg ()
 	{
-		StartCoroutine(MoveLeg_C());
+		moveCoroutine = StartCoroutine(MoveLeg_C());
 	}
 
 	public bool ShouldMove ()
@@ -48,7 +53,7 @@ public class ProceduralSpiderLegAnimator : MonoBehaviour
 	{
 		RaycastHit hit;
 		Vector3 newWantedPos = wantedTransform.position;
-		if (Physics.Raycast(IK.transform.position + (Vector3.up * 2), Vector3.down, out hit, 10f, LayerMask.GetMask("Environment")))
+		if (Physics.Raycast(IK.transform.position + (Vector3.up * 2) + forwardOffset, Vector3.down, out hit, 20f, LayerMask.GetMask("Environment")))
 		{
 			newWantedPos.y = hit.point.y;
 		}
@@ -61,14 +66,47 @@ public class ProceduralSpiderLegAnimator : MonoBehaviour
 		Vector3 startPosition = IK.Target.transform.position;
 		for (float i = 0; i < 1; i+= Time.deltaTime * (legSpeed * Random.Range(0.8f, 1.2f)))
 		{
-			Vector3 newPosition = Vector3.Lerp(startPosition, wantedTransform.transform.position, i / 1f);
+			Vector3 newPosition = Vector3.Lerp(startPosition, wantedTransform.transform.position + forwardOffset, i / 1f);
 			float yPos = Mathf.Lerp(startPosition.y, wantedTransform.transform.position.y, i / 1f);
 			yPos += heightCurve.Evaluate(i / 1f) * height;
 			newPosition.y = yPos;
 			IK.Target.transform.position = newPosition;
 			yield return null;
 		}
-		IK.Target.transform.position = wantedTransform.transform.position;
+		IK.Target.transform.position = wantedTransform.transform.position + forwardOffset;
+		isGrounded = true;
+		if (eventOnStep != "")
+		{
+			FeedbackManager.SendFeedback(eventOnStep, this);
+		}
+	}
+
+	public IEnumerator BumpLeg_C()
+	{
+		bool coroutineStopped = false;
+		if (moveCoroutine != null) {
+			coroutineStopped = true;
+			StopCoroutine(moveCoroutine);
+		}
+		isGrounded = false;
+		Vector3 startPosition = IK.Target.transform.position;
+		for (float i = 0; i < bumpDuration; i += Time.deltaTime * 1f)
+		{
+			IK.Target.transform.position = startPosition + (Vector3.up * (bumpHeightCurve.Evaluate(i / bumpDuration) * bumpHeight));
+			yield return null;
+		}
+		RaycastHit hit;
+		if (coroutineStopped && Physics.Raycast(IK.transform.position + (Vector3.up * 2) + forwardOffset, Vector3.down, out hit, 20f, LayerMask.GetMask("Environment")))
+		{
+			Vector3 startFallPosition = IK.transform.position;
+			Vector3 endFallPosition = startFallPosition;
+			endFallPosition.y = hit.point.y;
+			for (float i = 0; i < Vector3.Distance(IK.transform.position, hit.point); i += Time.deltaTime * 5f)
+			{
+				IK.Target.transform.position = Vector3.Lerp(startFallPosition, endFallPosition, i / Vector3.Distance(IK.transform.position, hit.point));
+				yield return null;
+			}
+		}
 		isGrounded = true;
 	}
 }
