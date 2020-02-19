1a4cb49d29eeb838affab83d08283b33556414c0
OneirosZzz <r.gremmo@rubika-edu.com>
2020-02-19 10:47:38 +0100

    cleaned up base folder

---
+++ Robot'em Up/Assets/Scripts/Miscellaneous/TempRotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempRotator : MonoBehaviour
+{
+	public float rotationSpeed = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+		transform.rotation = Quaternion.Euler(new Vector3(0, Time.time * rotationSpeed, 0)); 
+    }
+}
