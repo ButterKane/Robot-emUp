f3d63aa19ebe02b09d2bbfef979d9b28041979af
ButterKane <alexandre.fourquet@gmail.com>
2020-03-09 11:29:57 +0100

    feat: values of settings are now saved in dictionaries (one dico by setting "type")

--- Robot'em Up/Assets/Scripts/Levels and Menus/SliderUI.cs
+++ Robot'em Up/Assets/Scripts/Levels and Menus/SliderUI.cs
@@ -8,6 +8,7 @@ public class SliderUI : UIBehaviour
 {
     public Slider slider;
     [Range(0, 100)] public int defaultValue = 50;
+    public int currentValue;
     public TextMeshProUGUI valueText;
     private float defaultTimeBetweenChangeValue;
     private float timeBetweenValueChange;
@@ -17,6 +18,7 @@ public class SliderUI : UIBehaviour
     void Start()
     {
         slider.value = (float)defaultValue / 100;
+        currentValue = (int) slider.value * 100;
         timeBetweenValueChange = defaultTimeBetweenChangeValue;
         currentTimeProgressionBeforeValueChange = 0;
         UpdateSliderText();
@@ -45,12 +47,11 @@ public class SliderUI : UIBehaviour
         if (currentTimeProgressionBeforeValueChange <= 0)
         {
             slider.value += (float)_valueToAdd / 100;
+            currentValue = (int)slider.value * 100;
             currentTimeProgressionBeforeValueChange = timeBetweenValueChange;
             UpdateSliderText();
         }
-
         currentTimeProgressionBeforeValueChange -= Time.unscaledDeltaTime;
-
     }
 
     public override void IncreaseValue()
