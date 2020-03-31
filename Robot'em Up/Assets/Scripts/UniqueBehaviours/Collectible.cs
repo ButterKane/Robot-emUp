6211074816322e8a229cd302cf7d4da035d49936
Quentin <quentin.roze@gmail.com>
2020-03-31 11:34:41 +0200

    Update Collectible, all feedback done

--- Robot'em Up/Assets/Scripts/UniqueBehaviours/Collectible.cs
+++ Robot'em Up/Assets/Scripts/UniqueBehaviours/Collectible.cs
@@ -11,13 +11,19 @@ public class Collectible : MonoBehaviour
     [Header("References")]
     public Renderer myRend;
     public SphereCollider myCollider;
-    public TextMeshPro instructionText;
+    public GameObject collectibleUI;
     public Renderer quadPlayer1Rend;
     public Renderer quadPlayer2Rend;
+    public Animator myAnim;
 
     [Header("Variables to tweak")]
     public float ratioBeforeTriggerOut;
     public float timeToPressA;
+    //CompletionCircle !
+    public float normalEmissiveMultiplier;
+    public float maxEmissiveMultiplier;
+    public float curveTime;
+    public AnimationCurve emissiveCurve;
 
     [Header("Read Only")]
     [ReadOnly] public PlayerIndex playerIndex1;
@@ -30,8 +36,11 @@ public class Collectible : MonoBehaviour
     private GamePadState state2;
     Transform middlePointRefInRange;
     bool activated;
+    bool collected;
     float player1PressRatio;
     float player2PressRatio;
+    float emissiveCurveTimerPlayer1;
+    float emissiveCurveTimerPlayer2;
 
     void Awake()
     {
@@ -43,6 +52,12 @@ public class Collectible : MonoBehaviour
 
     void Update()
     {
+        CheckPlayerCompletion();
+        CompletionEmissiveUpdate();
+    }
+
+    void CheckPlayerCompletion()
+    {
         if (activated)
         {
             //UPDATE RATIO COMPLETION
@@ -51,7 +66,7 @@ public class Collectible : MonoBehaviour
             {
                 PlayerRatioUpdate(true, Mathf.Clamp01(player1PressRatio + Time.deltaTime / timeToPressA));
             }
-            else if(player1PressRatio>0)
+            else if (player1PressRatio > 0)
             {
                 PlayerRatioUpdate(true, Mathf.Clamp01(player1PressRatio - Time.deltaTime / timeToPressA));
             }
@@ -61,37 +76,67 @@ public class Collectible : MonoBehaviour
             {
                 PlayerRatioUpdate(false, Mathf.Clamp01(player2PressRatio + Time.deltaTime / timeToPressA));
             }
-            else if(player2PressRatio>0)
+            else if (player2PressRatio > 0)
             {
                 PlayerRatioUpdate(false, Mathf.Clamp01(player2PressRatio - Time.deltaTime / timeToPressA));
             }
 
             //CHECK IF FULLY COMPLETED
-            if(player1PressRatio >= 1 && player2PressRatio >= 1)
+            if (player1PressRatio >= 1 && player2PressRatio >= 1)
             {
                 CollectedEvent();
             }
 
 
         }
-        else if(player1PressRatio > 0 || player2PressRatio > 0)
+        else if (player1PressRatio > 0 || player2PressRatio > 0)
         {
             PlayerRatioUpdate(true, 0);
             PlayerRatioUpdate(false, 0);
         }
     }
 
+    void CompletionEmissiveUpdate()
+    {
+        //PLAYER1
+        if (player1PressRatio >= 1)
+        {
+            emissiveCurveTimerPlayer1 += Time.deltaTime/curveTime;
+            quadPlayer1Rend.material.SetFloat("_EmissiveMultiplier", Mathf.Lerp(normalEmissiveMultiplier, maxEmissiveMultiplier, emissiveCurve.Evaluate(emissiveCurveTimerPlayer1 % 1)));
+        }
+        else if (quadPlayer1Rend.material.GetFloat("_EmissiveMultiplier") > normalEmissiveMultiplier)
+        {
+            quadPlayer1Rend.material.SetFloat("_EmissiveMultiplier", Mathf.Lerp(quadPlayer1Rend.material.GetFloat("_EmissiveMultiplier"), normalEmissiveMultiplier, 0.2f));
+            emissiveCurveTimerPlayer1 = 0;
+        }
+
+        //PLAYER2
+        if (player2PressRatio >= 1)
+        {
+            emissiveCurveTimerPlayer2 += Time.deltaTime / curveTime;
+            quadPlayer2Rend.material.SetFloat("_EmissiveMultiplier", Mathf.Lerp(normalEmissiveMultiplier, maxEmissiveMultiplier, emissiveCurve.Evaluate(emissiveCurveTimerPlayer2 % 1)));
+        }
+        else if (quadPlayer2Rend.material.GetFloat("_EmissiveMultiplier") > normalEmissiveMultiplier)
+        {
+            quadPlayer2Rend.material.SetFloat("_EmissiveMultiplier", Mathf.Lerp(quadPlayer2Rend.material.GetFloat("_EmissiveMultiplier"), normalEmissiveMultiplier, 0.2f));
+            emissiveCurveTimerPlayer2 = 0;
+        }
+    }
+
     void PlayerRatioUpdate(bool _player1, float _value)
     {
-        if (_player1)
+        if (!collected)
         {
-            player1PressRatio = _value;
-            quadPlayer1Rend.material.SetFloat("_AddToCompleteCircle", player1PressRatio);
-        }
-        else
-        {
-            player2PressRatio = _value;
-            quadPlayer2Rend.material.SetFloat("_AddToCompleteCircle", player2PressRatio);
+            if (_player1)
+            {
+                player1PressRatio = _value;
+                quadPlayer1Rend.material.SetFloat("_AddToCompleteCircle", player1PressRatio);
+            }
+            else
+            {
+                player2PressRatio = _value;
+                quadPlayer2Rend.material.SetFloat("_AddToCompleteCircle", player2PressRatio);
+            }
         }
     }
 
@@ -108,7 +153,7 @@ public class Collectible : MonoBehaviour
                 player2InRange = true;
             }
 
-            if (!activated)
+            if (!activated && !collected)
             {
                 ActivateCollectible();
             }
@@ -128,7 +173,7 @@ public class Collectible : MonoBehaviour
                 player2InRange = false;
             }
 
-            if(!player1InRange && !player2InRange)
+            if(!player1InRange && !player2InRange && !collected)
             {
                 DeactivateCollectible();
             }
@@ -137,18 +182,24 @@ public class Collectible : MonoBehaviour
 
     void ActivateCollectible()
     {
-        instructionText.gameObject.SetActive(true);
+        collectibleUI.SetActive(true);
         activated = true;
     }
     void DeactivateCollectible()
     {
-        instructionText.gameObject.SetActive(false);
+        collectibleUI.SetActive(false);
         activated = false;
     }
 
     void CollectedEvent()
     {
-        print("HEY YO");
+        myAnim.enabled = true;
+        myAnim.SetTrigger("CollectedTrigger");
+        collected = true;
+    }
+
+    void DestroyObject()
+    {
         Destroy(gameObject);
     }
 }
