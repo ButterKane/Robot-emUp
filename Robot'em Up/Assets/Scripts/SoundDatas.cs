de937f5c6b17e0ed4a2810e7337a24fcc5f9fcb0
Marolles <julesduvette@gmail.com>
2020-04-09 15:49:11 +0200

    Sound manager update

--- Robot'em Up/Assets/Scripts/SoundDatas.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SoundDatas", menuName = "GlobalDatas/SoundDatas", order = 1)]
-public class SoundDatas : ScriptableObject
-{
-	public List<SoundData> soundList = new List<SoundData>(); 
-}
