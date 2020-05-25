3b24c2dd2a083b8ce45fa15c17827d95027d6e09
Marolles <julesduvette@gmail.com>
2020-05-25 21:47:42 +0200

    Reworked arena 3 scene

---
+++ Robot'em Up/Assets/DiodeManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiodeManager : MonoBehaviour
+{
+	public static Dictionary<int, List<DiodeController>> diodeDictionnary = new Dictionary<int, List<DiodeController>>();
+
+
+	public static void RegisterDiodeController(DiodeController dc, int id)
+	{
+		List<DiodeController> registeredControllers = new List<DiodeController>();
+		if (diodeDictionnary.ContainsKey(id)) {
+			registeredControllers = diodeDictionnary[id];
+		}
+		registeredControllers.Add(dc);
+		diodeDictionnary[id] = registeredControllers;
+	}
+
+	public static void ActivateDiodes ( int id )
+	{
+		List<DiodeController> registeredControllers = diodeDictionnary[id];
+		if (registeredControllers != null)
+		{
+			foreach (DiodeController dc in registeredControllers)
+			{
+				dc.Activate();
+			}
+		}
+	}
+}
