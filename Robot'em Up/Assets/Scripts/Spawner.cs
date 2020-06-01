e969c7212d26fefe9aa793fa04ed0abab6ef49bb
Marolles <julesduvette@gmail.com>
2020-06-01 16:55:18 +0200

    Reworked aerial spawner visuals and added new system for placing electrical plates (Just add the component "ElectricalPlateResizer" to any plate)

--- Robot'em Up/Assets/Scripts/Spawner.cs
+++ Robot'em Up/Assets/Scripts/Spawner.cs
@@ -168,8 +168,15 @@ public class Spawner : MonoBehaviour
 		if (type == SpawnerType.Air)
 		{
 			_enemy.gameObject.SetActive(false);
-			explosionVisualizer = new GameObject();
-			explosionVisualizer.AddComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("ExplosionVisualiseur");
+			explosionVisualizer = Instantiate(Resources.Load<GameObject>("ArenaResource/Spawners/P_ExplosionVisualizer"));
+			explosionVisualizer.transform.position = endPosition + Vector3.up * 0.01f;
+			explosionVisualizer.transform.rotation = Quaternion.LookRotation(Vector3.up);
+			IndianaExplosion ie = explosionVisualizer.GetComponent<IndianaExplosion>();
+			ie.myScale = _enemy.spawnImpactRadius * 0.33f;
+			ie.waitTimeForExplosion = zonePreviewDuration / 3f;
+			ie.Initiate();
+			/*
+			explosionVisualizer.AddComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("P_ExplosionVisualiseur");
 			explosionVisualizer.transform.position = endPosition + Vector3.up * 0.01f;
 			explosionVisualizer.transform.rotation = Quaternion.LookRotation(Vector3.up);
 			explosionVisualizer.transform.localScale = Vector3.one * 0.2f * _enemy.spawnImpactRadius;
@@ -179,7 +186,9 @@ public class Spawner : MonoBehaviour
 				explosionVisualizer.transform.localScale = Vector3.Lerp(Vector3.zero, explosionVisualizerMaxScale, i / (zonePreviewDuration / 3f));
 				yield return null;
 			}
-			yield return new WaitForSeconds(2 * (zonePreviewDuration / 3f));
+			*/
+			//yield return new WaitForSeconds(2 * (zonePreviewDuration / 3f));
+			yield return new WaitForSeconds(zonePreviewDuration / 3f);
 			_enemy.gameObject.SetActive(true);
 
 		}
