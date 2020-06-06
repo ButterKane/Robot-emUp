e10d5f9120aaa27977438bbbb1a335f2aedac2f9
lila <tite-pop2b@live.fr>
2020-06-06 17:29:30 +0200

    solve enemy shield bug

--- Robot'em Up/Assets/Scripts/Behaviours/BallBehaviour.cs
+++ Robot'em Up/Assets/Scripts/Behaviours/BallBehaviour.cs
@@ -456,7 +456,7 @@ public class BallBehaviour : MonoBehaviour
 					RaycastHit[] i_hitColliders = Physics.RaycastAll(transform.position, ballInformations.direction, ballInformations.moveSpeed * Time.deltaTime);
 					foreach (RaycastHit raycast in i_hitColliders)
 					{
-						EnemyShield i_selfRef = raycast.collider.GetComponentInParent<EnemyShield>();
+						/*EnemyShield i_selfRef = raycast.collider.GetComponentInParent<EnemyShield>();
 						if (i_selfRef != null)
 						{
 							if (i_selfRef.shield.transform.InverseTransformPoint(transform.position).z > 0.0)
@@ -465,7 +465,7 @@ public class BallBehaviour : MonoBehaviour
 								Vector3 i_newDirection = Vector3.Reflect(ballInformations.direction, i_selfRef.shield.transform.forward);
 								Bounce(i_newDirection, 1);
 							}
-						}
+						}*/
 
 						IHitable i_potentialHitableObjectFound = raycast.collider.GetComponent<IHitable>();
 						if (i_potentialHitableObjectFound != null && !hitGameObjects.Contains(i_potentialHitableObjectFound) && !isGhostBall)
