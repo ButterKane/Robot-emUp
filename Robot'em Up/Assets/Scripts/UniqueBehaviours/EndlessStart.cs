29298a3ed9a005c4b5e1639cd086825111a1d2dd
Mat <mat860@gmail.com>
2020-06-16 11:32:23 +0200

    Add Endless UI

---
+++ Robot'em Up/Assets/Scripts/UniqueBehaviours/EndlessStart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void  ActiveEndlessUI()
+    {
+        EndlessUI.instance.ShowUI();
+    }
+}
